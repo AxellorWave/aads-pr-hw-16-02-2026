b36acea443751846a280b66a5284f99c8f83d8ba
AxellorWave <Danya.Zharov.2006@gmail.com>
2026-03-05 14:02:29 +0300

    created main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-// оператор сравнения, оператор копирующего присваивания
+// Неявный интерфейс оператор сравнения, оператор копирующего присваивания
 
 template< class T >
 T * merge(const T * const * a, size_t sa, const size_t * sai, T * c)
@@ -28,4 +28,32 @@ T * merge(const T * const * a, size_t sa, const size_t * sai, T * c)
   }
   delete[] sai_cur;
   return c + size;
+}
+
+int main()
+{
+  size_t size_a = 7;
+  size_t size_b = 5;
+  size_t size_c = 6;
+  size_t sai[] {size_a, size_b, size_c};
+  int a[] {1, 2, 3, 4, 5, 6, 7};
+  int b[] {15, 56, 77, 81, 100};
+  int c[] {13, 21, 51, 54, 71, 100};
+  int* arr[]{a, b, c};
+
+  int* res = new int[size_a + size_b + size_c];
+  int* end = nullptr;
+  try {
+    end = merge(arr, 3, sai, res);
+  } catch (...) {
+    delete[] res;
+    return 1;
+  }
+
+  for (int* n = res; n < end; n++) {
+    std::cout << *n << ' ';
+  }
+  std::cout << "\n";
+  delete[] res;
+  return 0;
 }
